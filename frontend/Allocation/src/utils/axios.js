963e51124f9596d88fafb5e29bc80743affc7ed4
Krish Agarwal <agarwalkrish1222@gmail.com>
2026-02-24 13:39:22 +0530

    Clean up axios.js by removing comments
    Removed commented-out code for axios instance setup.

--- frontend/Allocation/src/utils/axios.js
+++ frontend/Allocation/src/utils/axios.js
@@ -1,18 +1,3 @@
-// // frontend/Allocation/src/utils/axios.js
-// import axios from "axios";
-
-// const instance = axios.create({
-//   baseURL: "https://project-desk-backend.onrender.com/api", // make sure this exactly matches Postman URL
-// });
-
-// instance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default instance;\
-
 // frontend/Allocation/src/utils/axios.js
 import axios from "axios";
 
